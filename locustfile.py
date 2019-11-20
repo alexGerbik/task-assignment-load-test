92289f093443d38f6267af2fc5f728bcc7150332
Alexander Gerbik <alexander.gerbik@gmail.com>
2019-11-20 20:05:22 +0300

    Create load testing script

---
+++ locustfile.py
@@ -0,0 +1,89 @@
+import random
+
+from locust import TaskSet, task, between, HttpLocust
+
+USERS_AMOUNT = 100
+readers_per_reviewer = 3
+do_pushback = False
+
+
+class LocustMixin:
+    host = 'http://localhost:5000'
+    # wait_time = between(0.001, 0.001)
+    wait_time = between(4, 5)
+
+
+class AbstractUserTaskSet(TaskSet):
+    headers = None
+    tasks_per_request = 4
+    logs_name = ''
+
+    def on_start(self):
+        self.login()
+
+    def login(self):
+        email, password = self.credentials.pop()
+        response = self.client.post("/auth/obtain-token", json={"email": email, "password": password})
+        token = response.json()["token"]
+        self.headers = {'Authorization': f'Bearer {token}'}
+
+    @task(100)
+    def accomplish_tasks(self):
+        url = "/task/request"
+        data = {"amount": self.tasks_per_request, "includeCurrentTasks": True}
+        with self.client.post(url, json=data, headers=self.headers, name=f"{self.logs_name} {url}", catch_response=True) as response:
+            tasks = response.json()['tasks']
+            if len(tasks) > 0:
+                response.success()
+            else:
+                response.failure("Task list is empty")
+        for t in tasks:
+            self.finish_task(t)
+
+    def finish_task(self, task):
+        id = task['taskId']
+        result = self.get_result(task)
+        data = {"final": True, "result": result, "timeSpent": 1}
+        self.client.post(f"/task/{id}/save", json=data, headers=self.headers, name=f"{self.logs_name} /task/save")
+
+
+class ReaderTaskSet(AbstractUserTaskSet):
+    credentials = []
+    logs_name = 'reader'
+
+    def setup(self):
+        reader_credentials = [(f"user_{i}@gmail.com", "password") for i in range(USERS_AMOUNT)]
+        self.credentials.clear()
+        self.credentials.extend(reader_credentials)
+
+    def get_result(self, task):
+        is_pushback = bool(task['previousSubmission'])
+        return task['taskData'] if is_pushback else {'coin': "HEAD" if random.random() < 0.5 else "TAIL"}
+
+
+class ReviewerTaskSet(AbstractUserTaskSet):
+    credentials = []
+    logs_name = 'reviewer'
+
+    def setup(self):
+        reviewer_credentials = [(f"reviewer_{i}@gmail.com", "password") for i in range(USERS_AMOUNT)]
+        self.credentials.clear()
+        self.credentials.extend(reviewer_credentials)
+
+    def get_result(self, task):
+        task_data = task['taskData']
+        is_rejectable = task_data['isRejectable']
+        inspected_task = task_data['inspectedTask']
+        if do_pushback and is_rejectable and random.random() < 0.10:
+            return {"decision": "reject", "feedback": {}}
+        return {"decision": "approve", "data": inspected_task}
+
+
+class ReaderUser(LocustMixin, HttpLocust):
+    weight = readers_per_reviewer
+    task_set = ReaderTaskSet
+
+
+class ReviewerUser(LocustMixin, HttpLocust):
+    weight = 1
+    task_set = ReviewerTaskSet
